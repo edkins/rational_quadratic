e8755ffc5e20404b112c9a76d1c657f808c2f24a
Giles Edkins <edkins@gmail.com>
2025-11-04 22:56:17 -0500

    x plot

--- frac.py
+++ frac.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 def to_denom(n:int) -> bool:
     return (2**n) - 1
@@ -22,17 +23,30 @@ def parity_str(p: np.ndarray) -> str:
     return result
 
 def main():
-    for n in range(2,10):
+    rangemax = 12
+    for n in range(2,rangemax+1):
         pairings = {}
         denom = to_denom(n)
         parities = np.zeros((denom+1, n-2), dtype=bool)
+        unreduced_ixs = []
+        reduced_ixs = []
         for a in range(denom+1):
+            if is_reduced(a,n):
+                reduced_ixs.append(a)
+            else:
+                unreduced_ixs.append(a)
             for m in range(2,n):
                 m_denom = to_denom(m)
                 count = (a * m_denom) // denom
                 parity = count % 2 != 0
                 parities[a, m-2] ^= parity
 
+        reduced_ixs = np.array(reduced_ixs)
+        full_parity_eq = (parities.reshape(1,denom+1,n-2) == parities.reshape(denom+1,1,n-2)).all(axis=-1)
+        parity_eq = full_parity_eq[:,reduced_ixs]
+        if n <= 9:
+            for row in parity_eq[reduced_ixs[:len(reduced_ixs)//2],:]:
+                print(parity_str(row[:len(row)//2]))
         for a in range(1,denom):
             if not is_reduced(a,n):
                 print(f"{a:3}/{denom}                  not_reduced")
@@ -50,8 +64,21 @@ def main():
                 dif = f"{pairings[a]-a:+3}"
             else:
                 dif = "   "
-            print(f"{a:3}/{denom} {parity_str(parities[a])} {pairings[a]:3}    {dif}")
+            n_eq = parity_eq[a,:].sum()
+
+            print(f"{a:3}/{denom} {parity_str(parities[a])} {pairings[a]:3}    {dif}    {n_eq:3}")
         print("-------")
+
+        if n == rangemax:
+            im = np.array(full_parity_eq, dtype=np.int8) + 1
+            for a,b in pairings.items():
+                assert im[a,b] == 2
+                im[a,b] = 5
+            for u in unreduced_ixs:
+                im[u,:] = 0
+                im[:,u] = 0
+            plt.imshow(im)
+            plt.show()
 
 if __name__ == "__main__":
     # print(is_reduced(2,2))
