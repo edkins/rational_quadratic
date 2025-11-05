3eb2a4c65d4c73af4a554a1cbc77532242e86cdc
Giles Edkins <edkins@gmail.com>
2025-11-04 22:12:30 -0500

    Better parity detection

--- frac.py
+++ frac.py
@@ -22,23 +22,35 @@ def parity_str(p: np.ndarray) -> str:
     return result
 
 def main():
-    for n in range(2,6):
+    for n in range(2,10):
+        pairings = {}
         denom = to_denom(n)
-        prev_parities = np.zeros(n-2, dtype=bool)
+        parities = np.zeros((denom+1, n-2), dtype=bool)
         for a in range(denom+1):
-            if not is_reduced(a,n):
-                continue
-
-            parities = np.zeros(n-2, dtype=bool)
             for m in range(2,n):
                 m_denom = to_denom(m)
                 count = (a * m_denom) // denom
                 parity = count % 2 != 0
-                parities[m-2] ^= parity
-            if a > 1:
-                print("    ", parity_str(parities ^ prev_parities))
-            prev_parities = parities
-            print(f"{a}/{denom}")
+                parities[a, m-2] ^= parity
+
+        for a in range(1,denom):
+            if not is_reduced(a,n):
+                print(f"{a:3}/{denom}                  not_reduced")
+                continue
+            if a not in pairings:
+                for b in range(a+1, denom):
+                    if not is_reduced(b,n):
+                        continue
+                    if np.array_equal(parities[b], parities[a]):
+                        pairings[a] = b
+                        pairings[b] = a
+                        break
+                assert a in pairings
+            if abs(pairings[a] - a) > 1:
+                dif = f"{pairings[a]-a:+3}"
+            else:
+                dif = "   "
+            print(f"{a:3}/{denom} {parity_str(parities[a])} {pairings[a]:3}    {dif}")
         print("-------")
 
 if __name__ == "__main__":
