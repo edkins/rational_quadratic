9708163aec6a1ced67cd579009f311dbef894da6
Giles Edkins <edkins@gmail.com>
2025-11-08 20:28:30 -0500

    More detailed investigation

--- drawing_board.py
+++ drawing_board.py
@@ -1,10 +1,12 @@
+from typing import Any
 from matplotlib import pyplot as plt
 import numpy as np
 
-MAXITER=200
+MAXITER=50
 BAILOUT=1024
-JULIA=False
+JULIA=True
 MANYPLOTS=False
+INVESTIGATE=True
 
 PALETTE = [
     None,
@@ -24,17 +26,53 @@ def frac(x: np.ndarray) -> np.ndarray:
 
 def main():
     if MANYPLOTS:
-        fig, ax = plt.subplots(4, 6, squeeze=False, figsize=(25,15))
+        mapping = {
+            (0,0): (0,0),
+            (0,1): (1,0),
+            (1,0): (0,1),
+            (1,1): (1,1),
+            (2,0): (0,2),
+            (2,1): (1,2),
+            (3,0): (0,3),
+            (3,1): (1,3),
+            (4,0): (0,4),
+            (4,1): (1,4),
+            (5,0): (0,5),
+            (5,1): (1,5),
+            (23,0): (2,0),
+            (23,1): (3,0),
+            (24,0): (2,1),
+            (24,1): (3,1),
+            (25,0): (2,2),
+            (25,1): (3,2),
+            (26,0): (2,3),
+            (26,1): (3,3),
+            (27,0): (2,4),
+            (27,1): (3,4),
+            (MAXITER-1,0): (2,5),
+            (MAXITER-1,2): (3,5),
+        }
+    elif INVESTIGATE:
+        mapping = {
+            (22,0): (0,0),
+            (22,1): (1,0),
+            (23,0): (0,1),
+            (23,1): (1,1),
+        }
     else:
-        fig, ax = plt.subplots(1, 1, squeeze=False, figsize=(25,15))
+        mapping = {
+            (MAXITER-1,2): (0,0)
+        }
+
+    fig, ax = plt.subplots(1+max(y for y,x in mapping.values()), 1+max(x for y,x in mapping.values()), squeeze=False, figsize=(25,15))
 
     num_tiles = 1 + MAXITER // 50
     for tile in range(num_tiles):
-        do_plot(tile, num_tiles, ax)
+        do_plot(tile, num_tiles, ax, mapping)
     plt.tight_layout()
     plt.show()
 
-def do_plot(tile: int, num_tiles: int, ax: np.ndarray):
+def do_plot(tile: int, num_tiles: int, ax: Any, mapping: dict):
     fractions = []
     for j in range(1,8):
         denom = (1<<j)-1
@@ -49,12 +87,12 @@ def do_plot(tile: int, num_tiles: int, ax: np.ndarray):
     fractions.sort(key=lambda x:x[0]/x[1])
 
     if JULIA:
-        width = 400 if MANYPLOTS else 2000
-        height = 400 if MANYPLOTS else 2000
-        xmin = -1.5
-        xmax = 1.5
-        ymin = -1.5
-        ymax = 1.5
+        width = 800 if MANYPLOTS else 2000
+        height = 800 if MANYPLOTS else 2000
+        xmin = -0.5
+        xmax = 0.5
+        ymin = -0.5
+        ymax = 0.5
     else:
         width = 700 if MANYPLOTS else 2800
         height = 300 if MANYPLOTS else 1200
@@ -109,11 +147,11 @@ def do_plot(tile: int, num_tiles: int, ax: np.ndarray):
         stuff[iters == i,:i+1] = stuff[iters == i,i::-1]
 
     pic = np.zeros_like(stuff[:,0], dtype=np.float64)
+
     for i in range(MAXITER):
-        py = 2*(i//6)
-        px = i%6
         angles = frac(np.atan2(np.imag(stuff[:,i]), np.real(stuff[:,i])) / 6.28318530718)
-        if MANYPLOTS and i < 12:
+        if (i,0) in mapping:
+            py,px = mapping[(i,0)]
             ax[py,px].imshow(angles.reshape(height,width), extent=(xmin,xmax,ymin,ymax), vmin=0, vmax=1)
             ax[py,px].axis("off")
             ax[py,px].set_ylim(full_ymin, full_ymax)
@@ -124,17 +162,21 @@ def do_plot(tile: int, num_tiles: int, ax: np.ndarray):
         else:
             pic[sel] = frac(0.5 * (pic + np.floor(2.0 * angles - pic + 0.5))[sel])
 
-        if MANYPLOTS and i < 12:
-            ax[py+1,px].imshow(pic.reshape(height,width), extent=(xmin,xmax,ymin,ymax), vmin=0, vmax=1)
-            ax[py+1,px].axis("off")
-            ax[py+1,px].set_ylim(full_ymin, full_ymax)
-    
-    rgb = pic.reshape(-1,1).repeat(3, axis=1)
-    for num,denom,j in fractions:
-        rgb[(pic >= num/denom-0.0001) & (pic <= num/denom+0.0001),:] = PALETTE[j]
-    ax[-1,-1].imshow(rgb.reshape(height,width,3), extent=(xmin,xmax,ymin,ymax))
-    ax[-1,-1].axis("off")
-    ax[-1,-1].set_ylim(full_ymin, full_ymax)
+        if (i,1) in mapping:
+            py,px = mapping[(i,1)]
+            ax[py,px].imshow(pic.reshape(height,width), extent=(xmin,xmax,ymin,ymax), vmin=0, vmax=1)
+            ax[py,px].axis("off")
+            ax[py,px].set_ylim(full_ymin, full_ymax)
+
+        if (i,2) in mapping:
+            rgb = pic.reshape(-1,1).repeat(3, axis=1)
+            for num,denom,j in fractions:
+                rgb[(pic >= num/denom-0.0001) & (pic <= num/denom+0.0001),:] = PALETTE[j]
+
+            py,px = mapping[(i,2)]
+            ax[py,px].imshow(rgb.reshape(height,width,3), extent=(xmin,xmax,ymin,ymax))
+            ax[py,px].axis("off")
+            ax[py,px].set_ylim(full_ymin, full_ymax)
 
 if __name__ == "__main__":
     main()
